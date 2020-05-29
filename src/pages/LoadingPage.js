6f04b76deb2478f35a3aadc9b1e89000bb848766
Munoon <Munoongg@gmail.com>
2020-05-29 12:08:43 +0300

    Add animation to loading page

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+var Spinner = require('react-native-spinkit');
 
 export function LoadingPage() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Loading...</Text>
+            <Spinner type='Wave' size={100} />
         </View>
     );
 }
